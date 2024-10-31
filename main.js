befb747c5bd0acad1c2cb40e11b6faae44a70cdc
LozzDev <lozzdeveloper@gmail.com>
2024-10-31 13:55:30 +0100

    added filter functionalities, imgs, minor changes

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ window.onload = function() {
     }, 2000); // 2000 ms = 2 segundos
   };
 
+
+
 //funcion asincrona
 
 async function fetchMoviesJson(){
@@ -24,10 +26,11 @@ async function fetchMoviesJson(){
          let year= movies.documentaries[i].year;
          let length= movies.documentaries[i].length;
          let director= movies.documentaries[i].director;
+         let season = movies.documentaries[i].season;
          let synopsis= movies.documentaries[i].synopsis;
 
          moviesSection.innerHTML +=`
-             <div class="card mx-auto mb-4" style="width: 18rem;">
+             <div class="card mx-auto mb-4 ${season}" style="width: 18rem;" >
                  <img src="${poster}" class="card-img-top mt-2  " alt="...">
                  <div class="card-body">
                  <h5 class="card-title">${title} - ${year}</h5>
@@ -37,6 +40,28 @@ async function fetchMoviesJson(){
          `
     }
  })
+
+ function zFilter(){
+    const superFilms=document.getElementsByClassName("Z");
+    const zImage = document.getElementById("zLogo");
+    const zFilms=document.getElementsByClassName("Super");
+
+    if(zImage.getAttribute("src") === "./images/Z-color.png"){
+        zImage.setAttribute("src", "./images/Z-blanco-y-negro.png");
+
+        for(let superFilm of superFilms){
+            superFilm.style.display="none";
+        }
+    }else if(zImage.getAttribute("src") === "./images/Z-blanco-y-negro.png"){   
+        zImage.setAttribute("src", "./images/Z-color.png");
+
+        for(let superFilm of superFilms){
+            superFilm.style.display="block";
+        }
+    }
+}     
+
+ 
 
 
         
